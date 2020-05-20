07baf35b343d3e018281fa1cb03e6511b3f05c6c
Manoel Tindão <timdao02@hotmail.com>
2020-05-20 15:47:54 -0300

    Teste do service com mock

--- src/test/java/com/tindao/minhasfinancas2/service/UsuarioServiceTest.java
+++ src/test/java/com/tindao/minhasfinancas2/service/UsuarioServiceTest.java
@@ -1,56 +1,50 @@
 package com.tindao.minhasfinancas2.service;
 
-import java.time.LocalDate;
-
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.tindao.minhasfinancas2.exception.RegraNegocioException;
-import com.tindao.minhasfinancas2.model.entity.Usuario;
 import com.tindao.minhasfinancas2.model.repository.UsuarioRepository;
+import com.tindao.minhasfinancas2.service.impl.UsuarioServiceImpl;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class UsuarioServiceTest 
 {
-	@Autowired
-	private UsuarioService usuarioService;
+	private UsuarioService service;
 	
-	@Autowired
-	private UsuarioRepository usuarioRepository;
+	@MockBean
+	private UsuarioRepository repository;
+	
+	@Before
+	public void setUp()
+	{
+		service = new UsuarioServiceImpl(repository);
+	}
 	
 	@Test(expected = Test.None.class)
 	public void deveValidarEmail()
 	{
 		//cenário
-		usuarioRepository.deleteAll();
-		
+		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(false);
+
 		//ação
-		usuarioService.validarEmail("email@email.com");
-		
+		service.validarEmail("email@email.com");
 	}
 	
 	@Test(expected = RegraNegocioException.class)
 	public void deveLancarErroAoValidarQuandoExistirEmailCadastrado()
 	{
 		//cenário
-		Usuario usuario = Usuario
-						.builder()
-						.nome("usuario")
-						.email("test@gmail.com")
-						.senha("123456")
-						.data(LocalDate.of(2020, 10, 05))
-						.build();
-		usuarioRepository.save(usuario);
+		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(true);
 		
 		//execução
-		usuarioService.validarEmail("test@gmail.com");
-		
+		service.validarEmail("test@gmail.com");
 	}
 	
 }
