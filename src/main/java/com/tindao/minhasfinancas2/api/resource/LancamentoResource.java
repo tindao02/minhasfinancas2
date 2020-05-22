af7033ce1b71af551108dcf2211f096ed0c51a4b
Manoel Tindão <timdao02@hotmail.com>
2020-05-21 23:30:10 -0300

    Lancamento controller método buscar

--- src/main/java/com/tindao/minhasfinancas2/api/resource/LancamentoResource.java
+++ src/main/java/com/tindao/minhasfinancas2/api/resource/LancamentoResource.java
@@ -1,15 +1,20 @@
 package com.tindao.minhasfinancas2.api.resource;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.persistence.Entity;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tindao.minhasfinancas2.api.dto.LancamentoDTO;
@@ -31,6 +36,35 @@ public class LancamentoResource
 	public LancamentoResource(LancamentoService service) 
 	{
 		this.service = service;
+	}
+	
+	@GetMapping
+	public ResponseEntity buscar(
+			@RequestParam(value = "descricao", required = false) String descricao,
+			@RequestParam(value = "mes", required = false) Integer mes,
+			@RequestParam(value = "ano", required = false) Integer ano,
+			@RequestParam("usuario") Long idUsuario
+			)
+	{
+		Lancamento lancamentoFiltro = new Lancamento();
+		lancamentoFiltro.setDescricao(descricao);
+		lancamentoFiltro.setMes(mes);
+		lancamentoFiltro.setAno(ano);
+		
+		Optional<Usuario> usuario = usuarioService.obterPorId(idUsuario);
+		
+		if(usuario.isPresent())
+		{
+			return ResponseEntity.badRequest().body("Não foi possível realizar a consulta. Usuário não encontrado para o Id informado");
+		}
+		else
+		{
+			lancamentoFiltro.setUsuario(usuario.get());
+		}
+		
+		List<Lancamento> lancamentos = service.buscar(lancamentoFiltro);
+		
+		return ResponseEntity.ok(lancamentos);
 	}
 	
 	@PostMapping
